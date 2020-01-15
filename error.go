0c956dbe4bdcaae486ecc8700107af686989a0b2
yinyihanbing <yinyihanbing@163.com>
2020-01-15 17:05:45 +0800

    增加奔溃恢复帮助函数

---
+++ error.go
@@ -0,0 +1,20 @@
+package goutils
+
+import (
+	"fmt"
+	"runtime"
+
+	"github.com/yinyihanbing/gutils/logs"
+)
+
+// 崩溃错误处理
+func PanicError() error {
+	if r := recover(); r != nil {
+		buf := make([]byte, 4096)
+		l := runtime.Stack(buf, false)
+		err := fmt.Errorf("%v: %s", r, buf[:l])
+		logs.Error(err)
+		return err
+	}
+	return nil
+}
